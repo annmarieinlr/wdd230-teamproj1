4bb9111255cf37eaeeb566546d40fc29d3a4c8c6
Lizzy Woodruff <woo19022@byui.edu>
2024-01-31 10:05:49 -0700

    removed fake comment

--- src/js/productList.mjs
+++ src/js/productList.mjs
@@ -15,7 +15,7 @@ function productCardtemplate(product) {
     >
   </li>`
 }
-//fake comment
+
 export default async function productList(selector, category) {
     
     // get the element we will insert the list into from the selector
