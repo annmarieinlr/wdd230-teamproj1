3b396df1fe20e08880ec7e2e2273d6fab2aa48ee
Shawn Yang <yangshawn14@gmail.com>
2024-02-12 10:12:47 -0700

    commit

--- src/js/productList.mjs
+++ src/js/productList.mjs
@@ -1,8 +1,15 @@
 import { getData } from "./productData.mjs";
 import { renderListWithTemplate } from "./utils.mjs";
 import { renderWithTemplate } from "./utils.mjs";
+
 function productCardtemplate(product) {
-    return `<li class="product-card">
+  // Calculate discounted price
+  const originalPrice = product.FinalPrice;
+  const discountPercentage = .2;
+  const discountAmount = originalPrice * discountPercentage;
+  const discountPrice = (1 - discountPercentage) * originalPrice;
+  console.log(discountPrice);
+  return `<li class="product-card">
     <a href="product_pages/index.html?product=${product.Id}">
     <img
       src="${product.Image}"
@@ -10,21 +17,22 @@ function productCardtemplate(product) {
     />
     <h3 class="card__brand">${product.Brand.Name}</h3>
     <h2 class="card__name">${product.NameWithoutBrand}</h2>
-    <p class="product-card__price">$${product.FinalPrice}</p></a>
+    <p class="product-card__price">$${discountPrice.toFixed(2)}</p>
+    <p class="product-card__discount">Save $${discountAmount.toFixed(2)} (${Math.round(20)}% off)</p>
   </li>`;
 }
 
 export default async function productList(selector, category) {
-    
-    // get the element we will insert the list into from the selector
-    const elem = document.querySelector(selector);
-    
-    // get the list of products 
-    const products = await getData(category);
-        //check this
-    console.log(products);
 
-    // render out the product list to the element
-    renderListWithTemplate(productCardtemplate, elem, products);
-    
+  // get the element we will insert the list into from the selector
+  const elem = document.querySelector(selector);
+
+  // get the list of products 
+  const products = await getData(category);
+  //check this
+  console.log(products);
+
+  // render out the product list to the element
+  renderListWithTemplate(productCardtemplate, elem, products);
+
 }
