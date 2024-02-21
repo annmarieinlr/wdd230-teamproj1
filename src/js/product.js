59d02736074ecd44b923e8a44fa733243ce7297d
Shawn Yang <yangshawn14@gmail.com>
2024-02-21 11:51:16 -0700

    Comment out import

--- src/js/product.js
+++ src/js/product.js
@@ -3,10 +3,7 @@ import { getLocalStorage } from "./utils.mjs";
 import { findProductById } from "./productData.mjs";
 import { getParam } from "./utils.mjs";
 import productDetails from "./productDetails.mjs";
-<<<<<<< HEAD
-import { loadHeaderFooter } from "./utils.mjs";
-=======
->>>>>>> 5eaa90950e4ed8adb3f98bd8d7b2dd7da2ecf8a9
+// import { loadHeaderFooter } from "./utils.mjs";
 
 // add to cart button event handler
 
