90da5752709938064c9298fa5b5f7e491509f655
Annmarie SanSevero <wor20005@byui.edu>
2024-02-23 17:52:05 -0600

    refactored code

--- src/js/product.js
+++ src/js/product.js
@@ -1,12 +1,5 @@
-// import { setLocalStorage } from "./utils.mjs";
-// import { getLocalStorage } from "./utils.mjs";
-// import { loadHeaderFooter } from "./utils.mjs";
-// import { findProductById } from "./productData.mjs";
 import { getParam } from "./utils.mjs";
 import productDetails from "./productDetails.mjs";
-// import { loadHeaderFooter } from "./utils.mjs";
-
-
 
 const productId = getParam("product");
 productDetails(productId);
