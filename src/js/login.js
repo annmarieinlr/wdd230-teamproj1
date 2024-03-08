74ed078670337b30a7ce9db0e3dd2b5e8a22e7f1
Annmarie SanSevero <wor20005@byui.edu>
2024-03-08 08:29:13 -0600

    fixed login redirection...Thank you, Br. Gedeborg

--- src/js/login.js
+++ src/js/login.js
@@ -2,7 +2,7 @@ import { getParam, loadHeaderFooter } from "./utils.mjs";
 import { login } from "./auth.mjs";
 
 loadHeaderFooter();
-const redirect = getParam("redirect");
+const redirect = getParam("redirect") || "/";
 
 document.querySelector("#loginButton").addEventListener("click", (e) => {
   const email = document.querySelector("#email").value;
